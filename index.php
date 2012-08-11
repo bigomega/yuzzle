41a355931e5bd4ccc16057cb1dc5ef6aae753f14
bharath <bharathraja@live.com>
2012-08-12 05:04:50 +0530

    a php file added | drag done

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<script type="text/javascript">
+
+</script>
+</head>
+<style>
+div{
+	height: 100;
+	width: 100;
+}
+
+div.pic1.piece{
+width:100;
+height:100;
+background: url('107.jpg') no-repeat scroll 0px 0px transparent;
+float: left;
+float: top;
+}
+
+div.pic2.piece{
+width:100;
+height:100;
+background: url('107.jpg') no-repeat scroll -100px 0px transparent;
+float: left;
+}
+
+
+div.pic3{
+width:100;
+height:100;
+background: url('107.jpg') no-repeat scroll 0px -100px transparent;
+position: relative;
+float:top;
+float: left;
+}
+
+div.pic4{
+width:100;
+height:100;
+background: url('107.jpg') no-repeat scroll -100px -100px transparent;
+float: left;
+}
+</style>
+<div class="pic1 peice"></div>
+<div class="pic2 piece"></div>
+<div class="piece"></div>
+<div class="pic4 piece"></div>
+<div class="pic4 piece"></div>
+
+</html>
